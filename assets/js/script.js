df6fbad9ae95f60816634c820016de12d0ae75be
geoffroyvilain <vilaingeoffroy02@gmail.com>
2019-01-28 14:22:03 +0100

    exo fini

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,14 @@
+//FONCTION POUR EFFACER LES VALEURS DES CHAMPS
 function efface() {
+// value='' --> met '' dans la donnée des champs
   document.getElementById('lastname').value = '';
   document.getElementById('firstname').value = '';
   document.getElementById('city').value = '';
 }
+/*
+//si on veut remettre les valeurs par defaut :
+//en html, remplacer la fonction efface par resetValues
+function resetValues(){
+  document.getElementById('informations').reset()
+}
+*/
